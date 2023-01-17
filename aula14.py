c5de4fe37ad97c7682428647aac011ab6128c570
Anderson <apschisky@gmail.com>
2023-01-17 10:56:33 -0400

    .

--- aula14.py
+++ aula14.py
@@ -1,6 +1,8 @@
 a = 'AAAAA'
 b = 'BBBBB'
 c = 1.1
-string = 'a={} b={} c={:.2f}'
-formato = string.format(a, b, c)
- 
+string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
+formato = string.format(
+    nome1=a, nome2=b, nome3=c)
+
+print(formato)
