b361ed6d37d39c9b64938f7dfa9b5a96f3e2d456
Anderson <apschisky@gmail.com>
2023-01-18 10:47:19 -0400

    python

---
+++ aula20.py
@@ -0,0 +1,10 @@
+valor1 = input('digite o primeiro valor: ')
+valor2 = input('digite o segundo valor: ')
+
+if valor1 == valor2:
+    print('Os valores digitados foram iguais')
+
+if valor1 > valor2:
+     print(f'O primeiro valor digitado "{valor1}", é maior que o segundo "{valor2}".')
+else:
+    print(f'O segundo valor digitado "{valor2}", é maior que o primeiro"{valor1}".')
---
+++ aula23.py
@@ -0,0 +1,15 @@
+# Operador lógico "not"
+# Usado para invertewr expressões
+# not True = False
+# not False = True
+
+# senha = input('Senha: ')
+
+# if not senha:
+#     print('Você não digitou nada')
+# elif senha == '123456':
+#     print('Acesso autorizado')
+
+
+print(not True) # False
+print(not False) # True
