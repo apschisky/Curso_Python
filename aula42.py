73756384c4a1663b6c4a746f99ac4d45275fe31e
Anderson <apschisky@gmail.com>
2023-01-25 10:54:29 -0400

    python

--- aula42.py
+++ aula42.py
@@ -1,5 +1,4 @@
-frase = 'O Python é uma linguagem de programação multiparadigma.' \
-    'Python foi criado por Guido van Rossum.'
+frase = input('Escreva uma frase: ')
 
 i = 0
 qtd_let = 0
@@ -7,12 +6,49 @@ letra_apareceu_mais = ''
 
 while i < len(frase):
     letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
     letra_apareceu = frase.count(letra_atual)
 
     if qtd_let < letra_apareceu:
         qtd_let = letra_apareceu
         letra_apareceu_mais = letra_atual
 
-i += 1
+    i += 1
 
-print('A letra que apareceu mais vezes')
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais}", apareceu '
+    f'{qtd_let}x.'
+
+)
+
+# frase = input('Escreva uma frase: ')
+
+# i = 0
+# qtd_apareceu_mais_vezes = 0
+# letra_apareceu_mais_vezes = ''
+
+# while i < len(frase):
+#     letra_atual = frase[i]
+
+#     if letra_atual == ' ':
+#         i += 1
+#         continue
+
+#     qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+
+#     if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
+#         qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+#         letra_apareceu_mais_vezes = letra_atual
+
+#     i += 1
+
+# print(
+#     'A letra que apareceu mais vezes foi '
+#     f'"{letra_apareceu_mais_vezes}" que apareceu '
+#     f'{qtd_apareceu_mais_vezes}x'
+# )
