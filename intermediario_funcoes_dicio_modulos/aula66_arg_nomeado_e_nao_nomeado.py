fa4930d4eae18162179fb3ffd6ea29b9432fca19
Anderson <apschisky@gmail.com>
2023-02-06 14:26:32 -0400

    intermediario

---
+++ intermediario_funcoes_dicio_modulos/aula66_arg_nomeado_e_nao_nomeado.py
@@ -0,0 +1,19 @@
+"""
+Argumentos nomeados e não nomeados em funções Python
+Argumento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor)
+"""
+
+
+
+def soma(x, y, z):
+    # Definição
+    print(f'{x=} y={y} {z=}', '|', 'x + y + z =', x + y + z)
+
+
+soma(1, 2, 3)
+soma(2, 1, 3)
+soma(y=2, x=1, z=3)
+soma(1, 2, z=5)
+
+print(1, 2, 3, sep='-')
