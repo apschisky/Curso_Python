7b8d874b353f026c1d3f4f1ed407328f896ae01c
Anderson <apschisky@gmail.com>
2023-02-06 14:59:32 -0400

    intermediario

--- intermediario_funcoes_dicio_modulos/aula68_escopo_funcoes_python copy.py
+++ intermediario_funcoes_dicio_modulos/aula68_escopo_funcoes_python copy.py
@@ -6,11 +6,23 @@ O escopo global é o escopo onde todo o código é alcançável.
 O escopo local é o escopo onde apenas nomes do mesmo local
  podem ser alcançados.
 """
+x= 1
 
 def escopo():
-    x = 1
+    global x
+    x = 10
+
+    def outra_funcao():
+        global x
+        x = 11
+        y = 2
+        print(x, y)
+
+    outra_funcao()
     print(x)
 
 
 
+print(x)
 escopo()
+print(x)
